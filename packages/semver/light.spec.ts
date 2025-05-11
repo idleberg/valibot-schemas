2f7820f333a0dbdd4311fe2de735a6bd87957bc8
Jan T. Sott <git@idleberg.com>
2025-05-11 23:57:32 +0200

    chore: update error message

--- packages/semver/light.spec.ts
+++ packages/semver/light.spec.ts
@@ -4,23 +4,27 @@ import { semver } from './light.ts';
 
 test('valid semver', () => {
 	const version = '8.0.8';
-	const { success } = safeParse(semver, version);
+	const { success } = safeParse(semver(), version);
 
 	expect(success).toBe(true);
 });
 
 test('invalid semver: short', () => {
 	const version = '1.0';
-	const { success } = safeParse(semver, version);
+	const { success } = safeParse(semver(), version);
 
 	expect(success).toBe(false);
-	expect(() => parse(semver, version)).toThrowError(`Invalid type: Expected Semantic Versioning received "${version}"`);
+	expect(() => parse(semver(), version)).toThrowError(
+		`Invalid type: Expected Semantic Versioning received "${version}"`,
+	);
 });
 
 test('invalid semver: long', () => {
 	const version = '6.1.7601.17514';
-	const { success } = safeParse(semver, version);
+	const { success } = safeParse(semver(), version);
 
 	expect(success).toBe(false);
-	expect(() => parse(semver, version)).toThrowError(`Invalid type: Expected Semantic Versioning received "${version}"`);
+	expect(() => parse(semver(), version)).toThrowError(
+		`Invalid type: Expected Semantic Versioning received "${version}"`,
+	);
 });
