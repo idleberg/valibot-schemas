84c929ac486304efc1ee07ebdde1b9299bd26012
Jan T. Sott <git@idleberg.com>
2025-05-11 23:54:11 +0200

    chore: rename import

--- packages/ksuid/index.spec.ts
+++ packages/ksuid/index.spec.ts
@@ -1,29 +1,29 @@
 import KSUID from 'ksuid';
 import { parse, safeParse } from 'valibot';
 import { expect, test } from 'vitest';
-import { ksuid as schema } from './index.ts';
+import { ksuid } from './index.ts';
 
 const validId = KSUID.randomSync().string;
 const invalidId = '@@@@@@@@@@@@@@@@@@@@@@@@@@@';
 
 test('valid ksuid', () => {
-	const { success } = safeParse(schema(), validId);
+	const { success } = safeParse(ksuid(), validId);
 
 	expect(success).toBe(true);
-	expect(() => parse(schema(), validId)).not.toThrowError();
+	expect(() => parse(ksuid(), validId)).not.toThrowError();
 });
 
 test('invalid ksuid', () => {
-	const { success } = safeParse(schema(), invalidId);
+	const { success } = safeParse(ksuid(), invalidId);
 
 	expect(success).toBe(false);
-	expect(() => parse(schema(), invalidId)).toThrowError(`Invalid type: Expected KSUID received "${invalidId}"`);
+	expect(() => parse(ksuid(), invalidId)).toThrowError(`Invalid type: Expected KSUID received "${invalidId}"`);
 });
 
 test('invalid ksuid, custom error string', () => {
 	const errorMessage = 'Custom error message';
 
-	const customSchema = schema(errorMessage);
+	const customSchema = ksuid(errorMessage);
 	const { success } = safeParse(customSchema, invalidId);
 
 	expect(success).toBe(false);
@@ -31,7 +31,7 @@ test('invalid ksuid, custom error string', () => {
 });
 
 test('invalid ksuid, custom error callback', () => {
-	const customSchema = schema((value) => `Custom error message, received ${value.received}`);
+	const customSchema = ksuid((value) => `Custom error message, received ${value.received}`);
 	const { success } = safeParse(customSchema, invalidId);
 
 	expect(success).toBe(false);
