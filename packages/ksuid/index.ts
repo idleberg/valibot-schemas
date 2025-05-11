2733baba782c0f2f236c23b61d9353ff266cd9ac
Jan T. Sott <git@idleberg.com>
2025-05-11 23:45:13 +0200

    refactor: update types

--- packages/ksuid/index.ts
+++ packages/ksuid/index.ts
@@ -1,5 +1,5 @@
 import KSUID from 'ksuid';
-import { type InferOutput, custom } from 'valibot';
+import { type InferOutput, type CustomIssue, custom, type ErrorMessage } from 'valibot';
 
 // additional check required since KSUID.parse() accepts invalid Base62 strings
 const BASE62_REGEX = /^[A-Za-z0-9+/]{27}$/;
@@ -8,7 +8,7 @@ const check = (value: unknown): boolean => {
 	return typeof value === 'string' && BASE62_REGEX.test(value) && KSUID.isValid(KSUID.parse(value).raw);
 };
 
-const defaultMessage = (value: { received: string }): string => {
+const defaultMessage = (value: CustomIssue): string => {
 	return `Invalid type: Expected KSUID received ${value.received}`;
 };
 
@@ -17,10 +17,10 @@ const defaultMessage = (value: { received: string }): string => {
  * @param overrideMessage - A string to override the default message or a callback to define a custom message function.
  * @returns A custom schema for KSUID validation.
  */
-export const ksuid = (overrideMessage?: string | ((value: { received: string }) => string)) => {
+export const ksuid = (overrideMessage?: string | ((value: CustomIssue) => string)) => {
 	const message = typeof overrideMessage === 'string' ? () => overrideMessage : overrideMessage || defaultMessage;
 
-	return custom<string>(check, message);
+	return custom<string, ErrorMessage<CustomIssue>>(check, message);
 };
 
 export type KsuidSchema = InferOutput<ReturnType<typeof ksuid>>;
